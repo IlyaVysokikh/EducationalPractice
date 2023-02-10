143204e9334260697be2893420f042926d4c19eb
IlyaVysokikh <vysokikh.ilya03@gmail.com>
2023-02-10 15:04:23 +0500

    create custom user model

--- educationalPractice/src/profiles/models.py
+++ educationalPractice/src/profiles/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class userNet(AbstractUser):
+    """Custom user model"""
+    middleName = models.CharField(max_length=50)
+    firstLogin = models.DateTimeField(null=True)
+    phone = models.CharField(max_length=14)
+    avatar = models.ImageField(upload_to='user/avatar/', blank=True, null=True)
+
+
